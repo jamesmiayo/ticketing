01575a281698cee1e6da17d16a5fc9f43bfdfb2d
Emmanuel Joshua A. Lemon <emmanuel.lemon@safc.com.ph>
2024-11-14 13:25:17 +0800

    Added Time finished attribute

--- Backend/app/Models/TicketHdr.php
+++ Backend/app/Models/TicketHdr.php
@@ -23,11 +23,20 @@ class TicketHdr extends Model
 
     protected $with = ['user:id,branch_id,name','sub_category:id,category_id,subcategory_description', 'sub_category.category:id,category_description', 'user.branch:id,branch_description'];
 
-    protected $appends = ['ticket_status'];
+    protected $appends = ['ticket_status' , 'time_finished'];
 
     protected $casts = [
-        'created_at' => 'datetime:Y-m-d H:i:s', // Custom format
+        'created_at' => 'datetime:Y-m-d H:i:s',
     ];
+
+    public function getTimeFinishedAttribute(){
+        $latestTicketLog = $this->ticket_logs_latest;
+
+    if ($latestTicketLog) {
+        return $this->created_at->diff($latestTicketLog->created_at)->format('%d days, %h hours, %i minutes, %s seconds');
+    }    return 'No related logs';
+
+    }
 
     public function getTicketStatusAttribute()
     {
@@ -53,7 +62,12 @@ class TicketHdr extends Model
 
     public function ticket_logs_latest()
     {
-        return $this->hasOne(TicketStatus::class , 'ticket_id')->latest();
+        return $this->hasOne(TicketStatus::class, 'ticket_id')->latestOfMany();
+    }
+
+    public function ticket_logs_completed()
+    {
+        return $this->hasOne(TicketStatus::class, 'ticket_id')->where('status', GlobalConstants::COMPLETED)->latestOfMany();
     }
 
     public static function getTicketLog()
