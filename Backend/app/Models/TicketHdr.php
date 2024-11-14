49504d98d4677ddec4b87a4c8cb66286d01f1b55
Emmanuel Joshua A. Lemon <emmanuel.lemon@safc.com.ph>
2024-11-14 13:28:12 +0800

    added time finished

--- Backend/app/Models/TicketHdr.php
+++ Backend/app/Models/TicketHdr.php
@@ -21,21 +21,22 @@ class TicketHdr extends Model
         'body'
     ];
 
-    protected $with = ['user:id,branch_id,name','sub_category:id,category_id,subcategory_description', 'sub_category.category:id,category_description', 'user.branch:id,branch_description'];
+    protected $with = ['user:id,branch_id,name', 'sub_category:id,category_id,subcategory_description', 'sub_category.category:id,category_description', 'user.branch:id,branch_description'];
 
-    protected $appends = ['ticket_status' , 'time_finished'];
+    protected $appends = ['ticket_status', 'time_finished'];
 
     protected $casts = [
         'created_at' => 'datetime:Y-m-d H:i:s',
     ];
 
-    public function getTimeFinishedAttribute(){
+    public function getTimeFinishedAttribute()
+    {
         $latestTicketLog = $this->ticket_logs_latest;
 
-    if ($latestTicketLog) {
-        return $this->created_at->diff($latestTicketLog->created_at)->format('%d days, %h hours, %i minutes, %s seconds');
-    }    return 'No related logs';
-
+        if ($latestTicketLog) {
+            return $this->created_at->diff($latestTicketLog->created_at)->format('%d days, %h hours, %i minutes, %s seconds');
+        }
+        return 'No related logs';
     }
 
     public function getTicketStatusAttribute()
@@ -51,7 +52,7 @@ class TicketHdr extends Model
 
     public function ticket_statuses()
     {
-        return $this->hasMany(TicketStatus::class , 'ticket_id' , 'id');
+        return $this->hasMany(TicketStatus::class, 'ticket_id', 'id');
     }
 
     public function sub_category()
