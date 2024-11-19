4543fc40b61c0e0a370493ad17cc810f595bfcfd
Dastine Jhay L. Bernardo <dastine.bernardo@safc.com.ph>
2024-11-19 13:52:41 +0800

    b

--- Frontend/src/modules/Ticketing/TicketList.tsx
+++ Frontend/src/modules/Ticketing/TicketList.tsx
@@ -91,7 +91,7 @@ const StatCard: React.FC<{
         p: 2,
         display: "flex",
         flexDirection: "column",
-        height: 100,
+        height: 140,
         background: backgroundColor, // Use the background color prop
         color: "white",
       }}
