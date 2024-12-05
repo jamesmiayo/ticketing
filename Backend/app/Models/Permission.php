58b76252146b415e1e137a718e7884c061dc319e
jamesmiayo <it_jamesmiayo25@yahoo.com>
2024-12-05 11:33:20 +0800

    permission

--- Backend/app/Models/Permission.php
+++ Backend/app/Models/Permission.php
@@ -3,8 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Models\Permission as SpatieRole;
 
-class Permission extends Model
+class Permission extends SpatieRole
 {
     use HasFactory;
 
