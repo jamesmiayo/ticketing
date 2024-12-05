ec6a7829b041605535811b9ca9d126cc9e6b9de0
Dastine Jhay L. Bernardo <dastine.bernardo@safc.com.ph>
2024-12-05 14:36:51 +0800

    new push

--- Frontend/src/api/configs/axiosConfigs.ts
+++ Frontend/src/api/configs/axiosConfigs.ts
@@ -4,37 +4,37 @@ import axios, {
   AxiosError,
   AxiosHeaders,
   AxiosResponse,
-} from 'axios'
+} from "axios";
 
 const apiClient: AxiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: "http://127.0.0.1:8000/api",
   headers: {
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${localStorage.getItem('token') || ''}`,
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${localStorage.getItem("token") || ""}`,
   },
   timeout: 10000,
   withCredentials: false,
-})
+});
 
 apiClient.interceptors.request.use(
   (config: InternalAxiosRequestConfig) => {
-    const token = localStorage.getItem('token')
+    const token = localStorage.getItem("token");
     if (token && config.headers instanceof AxiosHeaders) {
-      config.headers.set('Authorization', `Bearer ${token}`)
+      config.headers.set("Authorization", `Bearer ${token}`);
     }
-    return config
+    return config;
   },
   (error: AxiosError) => Promise.reject(error)
-)
+);
 
 apiClient.interceptors.response.use(
   (response: AxiosResponse) => response,
   (error: AxiosError) => {
     if (error.response && error.response.status === 401) {
-      window.location.href = '/login'
+      window.location.href = "/login";
     }
-    return Promise.reject(error)
+    return Promise.reject(error);
   }
-)
+);
 
-export default apiClient
+export default apiClient;
