341c4ed9aad422d1b07e141141aae2fdd84e6a98
Emmanuel Joshua A. Lemon <emmanuel.lemon@safc.com.ph>
2024-12-05 11:35:17 +0800

    Added env in frontend and fix auth login

--- Frontend/src/api/configs/axiosConfigs.ts
+++ Frontend/src/api/configs/axiosConfigs.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance, InternalAxiosRequestConfig, AxiosError, AxiosHeaders, AxiosResponse } from "axios";
 
 const apiClient: AxiosInstance = axios.create({
-  baseURL: 'http://192.168.10.214:8080/api',
+  baseURL: import.meta.env.VITE_BASE_URL + 'api',
   headers: {
     'Content-Type': 'application/json',
     Authorization: `Bearer ${localStorage.getItem("token") || ""}`,
