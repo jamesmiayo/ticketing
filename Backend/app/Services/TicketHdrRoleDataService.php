eece77e6b66efcbb06aff025cb76712c00035e72
Emmanuel Joshua A. Lemon <emmanuel.lemon@safc.com.ph>
2025-01-31 13:49:26 +0800

    Fixed Fetch user data

--- Backend/app/Services/TicketHdrRoleDataService.php
+++ Backend/app/Services/TicketHdrRoleDataService.php
@@ -74,9 +74,9 @@ class TicketHdrRoleDataService
                 });
                 break;
             default:
-                $ticketData = $this->ticketHdr->getTicketLog($data)->where(function ($query) use ($authUser , $ticketLogsFilter, $userId) {
-                    $query->whereHas('requestor', function ($subQuery) use ($authUser) {
-                        $subQuery->where('id', $authUser);
+                $ticketData = $this->ticketHdr->getTicketLog($data)->where(function ($query) use ($ticketLogsFilter, $userId) {
+                    $query->whereHas('requestor', function ($subQuery) use ($userId) {
+                        $subQuery->where('id', $userId);
                     })->orWhereHas('ticket_logs_latest.assignee', function ($subQuery) use ($userId) {
                             $subQuery->where('id', $userId);
                         })->orWhereHas('ticket_logs_latest', $ticketLogsFilter);;
